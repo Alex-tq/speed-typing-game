00fd50e50c2de43d9bd9b94ac048cd50d5be7d0a
unknown <alextq3036@gmail.com>
2021-10-23 14:34:11 -0400

    Updates

---
+++ precache-manifest.f95f14c4955f9f85e323179884e739ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ce9b2879a6047a33caa9ea3c20c5e5e",
+    "url": "/alex-tq.github.io/speed-typing-game/index.html"
+  },
+  {
+    "revision": "84198c095ae96fc50404",
+    "url": "/alex-tq.github.io/speed-typing-game/static/css/main.8f6c2cdd.chunk.css"
+  },
+  {
+    "revision": "7b8146e467ecb173a780",
+    "url": "/alex-tq.github.io/speed-typing-game/static/js/2.ee688ba6.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/alex-tq.github.io/speed-typing-game/static/js/2.ee688ba6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84198c095ae96fc50404",
+    "url": "/alex-tq.github.io/speed-typing-game/static/js/main.b8111906.chunk.js"
+  },
+  {
+    "revision": "66a139b6a7eb127e3a47",
+    "url": "/alex-tq.github.io/speed-typing-game/static/js/runtime-main.7b405255.js"
+  }
+]);
